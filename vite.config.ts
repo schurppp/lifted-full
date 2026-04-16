8cc8311a131d2a5765dc38e755ba76786951b405
schurppp <schurp.wwi25@student.dhbw-heidenheim.de>
2026-04-16 22:42:55 +0200

    Sprint 3: vite.config.ts - setupFiles auskommentiert bis setup.ts existiert
    Comment added to indicate future inclusion of setupFiles.

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./src/test/setup.ts'],
+    // setupFiles werden hinzugefuegt sobald src/test/setup.ts existiert (Sprint 5)
   },
 })
